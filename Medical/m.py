7a0cc12bc216f3e4b4fdf3542f0549405611e904
21MH1A04E2 <21MH1A04E2@acoe.edu.in>
2023-09-11 14:38:45 +0530

    first commit

---
+++ Medical/m.py
@@ -0,0 +1,21 @@
+# x = 10  # Example value, replace with your own
+# y = 2  # Example value, replace with your own
+# M = 3  # Example value, replace with your own
+
+# result = pow(x, y, M)
+# print(result)
+
+# n=float(input("enter %.2f"))
+# m=float(input("enter %.2f"))
+# print(n*m)
+n=int(input())
+a=n[0]
+b=n[1]
+c=n[2]
+if (a>=b):
+    largest=a
+elif (b>=a):
+    largest=b
+else:
+    largest =c
+print(largest)
